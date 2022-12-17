3122be3d3b33faccb526da783fd3023b98dffda5
Jomar Magdalaga <jmagdalaga5@gmail.com>
2022-12-17 13:48:31 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,16 +1,43 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.util.Objects;
 
-public class SimpleCalcGUI extends JFrame{
+public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfNumber1;
-    private JComboBox cbOperations;
+    private JComboBox<String> cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
-    private JLabel lblResult;
+    private JTextField lblResult;
 
+    public SimpleCalcGUI() {
+        setTitle("Simple Calculator");
+
+        btnCompute.addActionListener((event) -> {
+
+                double num1 = Double.parseDouble(tfNumber1.getText());
+                double num2 = Double.parseDouble(tfNumber2.getText());
+                String operation = (String) cbOperations.getSelectedItem();
+
+                double result = 0;
+
+                // Compute the result based on the chosen operator
+                switch (Objects.requireNonNull(operation)) {
+                    case "+" -> result = num1 + num2;
+                    case "-" -> result = num1 - num2;
+                    case "*" -> result = num1 * num2;
+                    case "/" -> result = num1 / num2;
+                }
+
+            lblResult.setText(Double.toString(result));
+        });
+    }
     public static void main(String[] args) {
-
+        SimpleCalcGUI calculator = new SimpleCalcGUI();
+        calculator.setContentPane(new SimpleCalcGUI().panel1);
+        calculator.setSize(600, 300);
+        calculator.setVisible(true);
+        calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
